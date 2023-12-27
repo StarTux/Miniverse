8e30d864049077afadeb69cdea4bb342cf43bcb9
StarTux <ths.loy@gmail.com>
2023-12-27 18:57:30 +0100

    Comment

--- src/main/java/com/cavetale/miniverse/Miniverse.java
+++ src/main/java/com/cavetale/miniverse/Miniverse.java
@@ -200,7 +200,7 @@ public final class Miniverse {
                         }
                         Material mat = materialMap.of(block.getType());
                         switch (mat) {
-                            //case GRASS:
+                            //case SHORT_GRASS:
                             //case SEAGRASS:
                         case TALL_SEAGRASS:
                             //case KELP_PLANT:
